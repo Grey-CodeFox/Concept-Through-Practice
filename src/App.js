6b65a9b8f1509a9183cd6f50b9d13d0bfb62d68c
arjun balachandran <arjunbalachandranofficial@gmail.com>
2024-02-27 20:31:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import { data } from './data'
+import List from './List'
+function App() {
+  const [people, setPeople] = useState(data)
+  const clickHandler = () => {
+    setPeople([])
+  }
+
+  const [names, setNames] = useState('')
+  const [ages, setAges] = useState(0)
+  const changeNameHandler = (event) => {
+    setNames(event.target.value)
+  }
+  const changeAgeHandler = (event) => {
+    setAges(event.target.value)
+  }
+
+  const submitHandler = () => {
+    const newData = { id: people.length + 1, name: names, age: ages }
+
+    setPeople([...people, newData])
+    setNames('')
+    setAges()
+  }
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthday's Today</h3>
+        <List people={people} />
+        <button onClick={clickHandler}>Clear</button>
+      </section>
+      <section className="container">
+        <h3>Enter New Member</h3>
+        <label htmlFor="name">Name : </label>
+        <input
+          type="text"
+          id="name"
+          value={names}
+          onChange={changeNameHandler}
+        />
+        <br />
+        <label htmlFor="age">Age : </label>
+        <input
+          type="number"
+          id="age"
+          value={ages}
+          onChange={changeAgeHandler}
+        />
+
+        <button onClick={submitHandler}>Submit</button>
+      </section>
+    </main>
+  )
+}
+
+export default App
